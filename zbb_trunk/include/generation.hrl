bcfb9604fd85ba489ef1a8b0aa62ad03e1b63683
2022581577 <2022581577@qq.com>
2016-02-27 19:54:38 +0800

    删除一些东西

--- zbb_trunk/include/generation.hrl
+++ zbb_trunk/include/generation.hrl
@@ -1,13 +1,10 @@
 -include("generation_util.hrl").
 
--record(durable_record,{name,               %% Record名称
+-record(durable_record,{name,               %% Record名称，需要与表名相同
                         keypos = 1,         %% 主键名称，如果为列表，则填写整数 1,2,3
-                        db_keypos = 1,      %% 某些表可能需要不同的db_keypos
-                        ets_type = set,     %% ets类型
                         is_preload = false, %% 是否已经预先加载(针对公共数据，一般都已经预加载了的)
                         is_user = false,    %% 是否玩家相关数据
-                        record_list = false,%% 是否record list 格式,存库优化
-                        record_fields = false
+                        record_list = false %% 是否record list 格式,存库优化
                     }).
 
 %% 内存数据库数据定义
