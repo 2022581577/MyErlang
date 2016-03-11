8c9832262a1bfeedea900419ab81a55e0182a8aa
2022581577 <2022581577@qq.com>
2016-03-12 02:28:15 +0800

    send xiuga

--- zbb_trunk/src/game/game_pack_send.erl
+++ zbb_trunk/src/game/game_pack_send.erl
@@ -17,11 +17,19 @@
         ,nodelay_send_to_one/3
         ,send_to_users/3
         ,nodelay_send_to_users/3
+
+        ,send_to_all/1
         ,send_to_all/2
         ,cross_send_to_all/2
+
+        ,send_to_map_all/2
         ,send_to_map_all/3
+        ,map_send_to_all/2
         ,map_send_to_all/3
+
+        ,send_to_map_area/4
         ,send_to_map_area/5
+        ,map_send_to_area/4
         ,map_send_to_area/5
     ]).
 
@@ -69,10 +77,13 @@ nodelay_send_to_users(UserXList, Cmd, Data) ->
 
 
 %% @doc 当前节点给所有人发消息
-send_to_all(Cmd, Data) ->
-    {ok, Bin} = pack(2, Cmd, Data), %% 默认广播至少2个人
+send_to_all(CmdDataList) ->
+    %% 默认广播至少2个人
+    Bin = [begin {ok, PackBin} = pack(2, Cmd, Data), PackBin end || {Cmd, Data} <- CmdDataList],
     L = ets:tab2list(?ETS_USER_ONLINE),
     [send_to_one(Pid, Bin) || #user_online{pid = Pid} <- L].
+send_to_all(Cmd, Data) ->
+    send_to_all([{Cmd, Data}]).
 
 %% @doc 在跨服节点给所有节点广播
 cross_send_to_all(Cmd, Data) ->
@@ -82,38 +93,49 @@ cross_send_to_all(Cmd, Data) ->
 
 
 %% @doc 非地图进程发消息给所有人
+send_to_map_all(MapPid, [{_Cmd, _Data} | _} = CmdDataList) ->
+    srv_map:cast_state_apply(MapPid, {?MODULE, map_send_to_all, [CmdDataList]}).
 send_to_map_all(MapPid, Cmd, Data) ->
-    srv_map:cast_state_apply(MapPid, {?MODULE, map_send_to_all, [Cmd, Data]}).
+    send_to_map_all(MapPid, [{Cmd, Data}]).
+    %%srv_map:cast_state_apply(MapPid, {?MODULE, map_send_to_all, [Cmd, Data]}).
 
 %% @doc 在地图进程中发送消息给所有人
-map_send_to_all(#map{map_id = MapID, user_dict = UserDict}, Cmd, Data) ->
+map_send_to_all(#map{map_id = MapID, user_dict = UserDict}, CmdDataList) ->
     Size = dict:size(UserDict),
-    {ok, Bin} = pack(Size, Cmd, Data),
+    Bin = [begin {ok, PackBin} = pack(Size, Cmd, Data), PackBin end || {Cmd, Data} <- CmdDataList],
     case data_map:get(MapID) of
         #tpl_map{map_cross_type = 0} -> %% 非跨服地图
             [nodelay_send_to_one(E, Bin) || #map_user{user_pid = E} <- dict:to_list(UserDict)];
         _ ->        %% TODO 跨服地图，看是否需要把不同节点的玩家区分出来，先发送到对应节点在
             [nodelay_send_to_one(E, Bin) || #map_user{user_pid = E} <- dict:to_list(UserDict)]
     end.
+map_send_to_all(#map{map_id = MapID, user_dict = UserDict}, Cmd, Data) ->
+    map_send_to_all(Map, [{Cmd, Data}]).
 
 
 %% @doc 非地图进程发消息给某个区域内的人
+send_to_map_area(MapPid, X, Y, [{_Cmd, _Data} | _] = CmdDataList) ->
+    srv_map:cast_state_apply(MapPid, {?MODULE, map_send_to_area, [X, Y, CmdDataList]}).
 send_to_map_area(MapPid, X, Y, Cmd, Data) ->
-    srv_map:cast_state_apply(MapPid, {?MODULE, map_send_to_area, [X, Y, Cmd, Data]}).
+    send_to_map_area(MapPid, X, Y, [{Cmd, Data}]).
+%%    srv_map:cast_state_apply(MapPid, {?MODULE, map_send_to_area, [X, Y, Cmd, Data]}).
 
 %% @doc 在地图进程中发送消息给某个区域的人
-map_send_to_area(#map{map_id = MapID, aoi = Aoi}, X, Y, Cmd, Data) ->
+map_send_to_area(#map{map_id = MapID, aoi = Aoi}, X, Y, CmdDataList) ->
     %% 根据x,y获取区域内9宫格
     GridList = map_aoi:get_grids(X, Y),
     AoiObjList = map_aoi:get_grids_object(Aoi, GridList, ?AOI_OBJ_TYPE_USER),
     Len = length(AoiObjList),
-    {ok, Bin} = pack(Len, Cmd, Data),
+    Bin = [begin {ok, PackBin} = pack(Size, Cmd, Data), PackBin end || {Cmd, Data} <- CmdDataList],
     case data_map:get(MapID) of
         #tpl_map{map_cross_type = 0} -> %% 非跨服地图
             [nodelay_send_to_one(E, Bin) || #aoi_obj{pid = E} <- AoiObjList];
         _ ->        %% TODO 跨服地图，看是否需要把不同节点的玩家区分出来，先发送到对应节点在
             [nodelay_send_to_one(E, Bin) || #aoi_obj{pid = E} <- AoiObjList]
     end.
+map_send_to_area(Map, X, Y, Cmd, Data) ->
+    map_send_to_area(Map, X, Y, [{Cmd, Data}]).
+
 
 
 %% @doc TODO 地图中阵营广播(涉及到地图的阵营)
