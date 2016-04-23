5b714a8aa691721eda9c5e7c330a84df1eb124d3
2022581577 <2022581577@qq.com>
2016-04-23 15:15:03 +0800

    删除一些日志

--- src/user/user_routing.erl
+++ src/user/user_routing.erl
@@ -4,18 +4,18 @@
 %%% @desc   : 玩家数据加载
 %%%----------------------------------------------------------------------
 
--module(user_load).
+-module(user_routing).
 -include("common.hrl").
 -include("record.hrl").
 
--export([routing/1]).
+-export([routing/3]).
 
 routing(User, Cmd, Data) ->
     case Cmd div 1000 of
         11 ->
             pp_user:handle(User, Data);
         _ ->
-            ?WARNING("No Hnadle Cmd:~w,Bin:~w,UserStatus:~w",[Cmd,Bin,UserStatus]),
+            ?WARNING("No Hnadle User:~w, Cmd:~w, Data:~w",[User, Cmd, Data]),
             srv_user:cast_stop(self(), routing_cmd),
             {ok, User}
     end.
